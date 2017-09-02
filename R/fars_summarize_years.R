2a194b84db22745aeff8b255483c6750c89386b1
Dong-Seok Yim <yimds@catholic.ac.kr>
2017-09-03 08:58:53 +0900

    importFrom added.

--- R/fars_summarize_years.R
+++ R/fars_summarize_years.R
@@ -1,8 +1,11 @@
 #' Summary
 #' using datafile of a specific year, the number of accidents per month are presented.
 #'
+#' @param year The year of the accidents recorded
 #' @inheritParams fars_read_years
 #' @return This function returns the number of accidents per month in the accident file
+#' @importFrom dplyr bind_rows group_by summarize
+#' @importFrom tidyr spread
 #'
 #' @export
 fars_summarize_years <- function(years) {
