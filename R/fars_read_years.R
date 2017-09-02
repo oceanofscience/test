198072dfbfc3761d1c7b21d2dd7ab7328405b9fb
Dong-Seok Yim <yimds@catholic.ac.kr>
2017-09-03 08:58:39 +0900

    importFrom added.

--- R/fars_read_years.R
+++ R/fars_read_years.R
@@ -3,8 +3,10 @@
 #' This function harvests data from the file for summary or drawing maps.
 #' Users don't need to do this separately, thus this function is not exported.
 #'
+#' @param year The year of the accidents recorded
 #' @inheritParams make_filename
 #' @return This function returns month data of the accidents in the accident file
+#' @importFrom dplyr mutate select
 fars_read_years <- function(years) {
         lapply(years, function(year) {
                 file <- make_filename(year)
